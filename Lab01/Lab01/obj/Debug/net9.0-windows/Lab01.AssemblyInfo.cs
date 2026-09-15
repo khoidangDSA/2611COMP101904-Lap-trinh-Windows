8bc2b29317882894a7ad0d566863994dfc179da9
Đặng Nguyên Khôi <khoidang2442007@gmail.com>
2026-09-15 15:57:28 +0700

    fix images

--- Lab01/Lab01/obj/Debug/net9.0-windows/Lab01.AssemblyInfo.cs
+++ Lab01/Lab01/obj/Debug/net9.0-windows/Lab01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508ca156fb82bd9cead0aeaf4bf220a86436014a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471342daf5e654948375be4b22d3337d8397b8e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
