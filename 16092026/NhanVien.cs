33a98f85e1b56d180a5123bc5d1aed87ca9bd367
Đặng Nguyên Khôi <khoidang2442007@gmail.com>
2026-09-20 20:03:12 +0700

    Them bai tap quan ly nhan vien 16092026

---
+++ 16092026/NhanVien.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16092026
+{
+    internal class NhanVien
+    {
+        private string maNV;
+        private string hoTen;
+        private double luongCoBan;
+        public NhanVien(string maNV, string hoTen,double luongCoBan)
+        {
+            MaNV = maNV;
+            HoTen = hoTen;
+            LuongCoBan = luongCoBan;
+        }
+        public string MaNV
+        {
+            get { return maNV; }
+            set { maNV = value; }
+        }
+
+        public string HoTen
+        {
+            get { return hoTen; }
+            set { hoTen = value; }
+        }
+
+        public double LuongCoBan
+        {
+            get { return luongCoBan; }
+
+            set {
+                if (value > 0) { luongCoBan = value; }
+                else {
+                    Console.WriteLine("Lương cơ bản phải > 0");
+                }
+            }
+        }
+        //Phương thức virtual tính lương
+        public virtual double TinhLuong()
+        {
+            return luongCoBan;
+        }
+        // Phương thức virtual hiển thị thông tin
+        public virtual void HienThiThongTin()
+        {
+            Console.WriteLine($"Mã Nhân viên: {MaNV}");
+            Console.WriteLine($"Tên Nhân Viên: {HoTen}");
+            Console.WriteLine($"Lương cơ bản: {LuongCoBan}");
+        }
+        
+
+        
+        
+    }
+}
---
+++ 16092026/NhanVienThoiVu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16092026
+{
+    internal class NhanVienThoiVu : NhanVien
+    {
+        private int soGioLam;
+        private double luongTheoGio;
+
+        public NhanVienThoiVu(string maNV, string hoTen,double luongCoBan, int soGioLam, double luongTheoGio)
+            : base(maNV, hoTen, luongCoBan)
+        {
+            SoGioLam = soGioLam;
+            LuongTheoGio = luongTheoGio;
+        }
+        public int SoGioLam
+        {
+            get { return soGioLam; }
+            set
+            {
+                if (value >= 0) soGioLam = value;
+                else Console.WriteLine("Số giờ làm phải lớn hơn hoặc bằng 0.");
+            }
+        }
+        public double LuongTheoGio
+        {
+            get { return luongTheoGio; }
+            set
+            {
+                if (value > 0) luongTheoGio = value;
+                else Console.WriteLine("Lương theo giờ phải lớn hơn hoặc bằng 0.");
+            }
+        }
+        public override double TinhLuong()
+        {
+            return SoGioLam * LuongTheoGio;
+        }
+        public override void HienThiThongTin()
+        {
+            base.HienThiThongTin(); 
+            Console.WriteLine($"Số giờ làm: {SoGioLam}"); 
+            Console.WriteLine($"Lương theo giờ: {LuongTheoGio}"); 
+            Console.WriteLine($"Lương: {TinhLuong()}");
+        }
+    }
+}
